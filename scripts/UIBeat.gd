867eb8298897adcba84cd77c4463f4eeb5696e8e
Jerry Yuan Chen <jerrych.97@gmail.com>
2018-01-20 18:18:12 -0500

    add switching queue

--- scripts/UIBeat.gd
+++ scripts/UIBeat.gd
@@ -1,6 +1,13 @@
 extends Sprite
 
-signal beat
+var fade_speed = 2
 
 func _ready():
-	pass
+	set_process(true)
+func _process(delta):
+	var opacity = get_self_opacity()
+	if opacity > 0:
+		set_self_opacity(opacity - delta*fade_speed)
+
+func show_blur(opacity):
+	set_self_opacity(opacity)
