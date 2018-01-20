33e4e89068b56ded33df6fc6768074701a049574
VictoryShoe <victoryshoe@hotmail.com>
2018-01-20 17:22:00 -0500

    More fixes

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,14 +1,13 @@
 extends Camera2D
 
 var screen_width = Globals.get("display/width")
-onready var player = get_node("../")
+onready var player_pos = get_node("../").get_global_pos()
 
 func ready():
-	var player_pos = player.get_global_pos()
 	var new_pos = Vector2(screen_width / 2, player_pos.y)
 	set_global_pos(new_pos)
 
-func _fixed_process(delta):
-	var player_pos = player.get_global_pos()
+func _process(delta):
+	player_pos = get_node("Player").get_global_pos()
 	var new_pos = Vector2(screen_width / 2, player_pos.y)
 	set_global_pos(new_pos)
--- scripts/Collector.gd
+++ scripts/Collector.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+onready var root_camera = get_tree().get_root().get_node("Map/Camera2D")
+
 func _ready():
 	connect("body_enter", self, "_on_object_enter")	
 	
@@ -7,6 +9,7 @@ func _on_object_enter(body):
 	print(body, " touched the wall...")
 	if body.is_in_group("player"):
 		print("GAME OVER")
+		root_camera.make_current()
 		body.free()
 	elif body.is_in_group("obstacle"):
 		body.free()
