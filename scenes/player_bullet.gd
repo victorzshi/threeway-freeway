cc9720858a1a2433ecd90c701a2842fbeff5dc51
VictoryShoe <victoryshoe@hotmail.com>
2018-01-21 02:05:33 -0500

    Add explosions and screen shake

--- scenes/player_bullet.gd
+++ scenes/player_bullet.gd
@@ -5,6 +5,12 @@ export var speed = 1000
 var timer
 onready var crate_explosion = preload("res://scenes/CrateExplosion.tscn")
 
+# Screen shake variables
+onready var camera = get_tree().get_root().get_node("Map/Player/Camera2D")
+var magnitude = 13
+var duration = 1
+var time = 0
+
 func _ready():
 	set_fixed_process(true)
 	connect("body_enter", self, "_on_object_enter")
@@ -28,6 +34,7 @@ func destroy():
 func _on_object_enter(body):
 	if body.is_in_group("obs_destruct"):
 		# play explosion
+		shake()
 		var explode = crate_explosion.instance()
 		explode.set_global_pos(body.get_global_pos())
 		explode.set_emitting(true)
@@ -36,3 +43,16 @@ func _on_object_enter(body):
 		destroy()
 	elif body.is_in_group("obstacle"):
 		destroy()
+
+func shake():
+	while time < duration:
+		time += get_process_delta_time()
+		
+		var offset = Vector2()
+		randomize()
+		offset.x = rand_range(-magnitude, magnitude)
+		#offset.y = rand_range(-magnitude, magnitude)
+		camera.set_offset(offset)
+		
+		yield(get_tree(), "idle_frame")
+	time = 0
--- scripts/Collector.gd
+++ scripts/Collector.gd
@@ -3,6 +3,12 @@ extends Area2D
 onready var ship_explosion = preload("res://scenes/ShipExplosion.tscn")
 onready var crate_explosion = preload("res://scenes/CrateExplosion.tscn")
 
+# Screen shake variables
+onready var camera = get_tree().get_root().get_node("Map/Player/Camera2D")
+var magnitude = 60
+var duration = 1.5
+var time = 0
+
 func _ready():
 	connect("body_enter", self, "_on_object_enter")	
 	
@@ -13,6 +19,7 @@ func _on_object_enter(body):
 		
 		# Hide ship and show explosion
 		body.hide()
+		shake()
 		var explode = ship_explosion.instance()
 		explode.set_global_pos(body.get_global_pos())
 		explode.set_emitting(true)
@@ -21,7 +28,7 @@ func _on_object_enter(body):
 		global.last_score = global.current_distance
 		
 		var t = Timer.new()
-		t.set_wait_time(1)
+		t.set_wait_time(3)
 		t.set_one_shot(true)
 		self.add_child(t)
 		t.start()
@@ -36,3 +43,16 @@ func _on_object_enter(body):
 		explode.set_emitting(true)
 		get_node("../../").add_child(explode)
 		body.free()
+
+func shake():
+	while time < duration:
+		time += get_process_delta_time()
+		
+		var offset = Vector2()
+		randomize()
+		offset.x = rand_range(-magnitude, magnitude)
+		offset.y = rand_range(-magnitude, magnitude)
+		camera.set_offset(offset)
+		
+		yield(get_tree(), "idle_frame")
+	time = 0
