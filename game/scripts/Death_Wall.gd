1145054a5912ef353f4115c65da6f06950652b92
Victor Shi <victoryshoe@gmail.com>
2018-01-26 13:24:10 -0500

    Update wall speed at long distance

--- game/scripts/Death_Wall.gd
+++ game/scripts/Death_Wall.gd
@@ -2,11 +2,15 @@ extends KinematicBody2D
 
 var SPEED = 2
 var MIN_DISTANCE = 600
+var DEATH_DISTANCE = 15400 #offset 400 for initial player spawn
 onready var player = get_node("../Player")
 
 func _fixed_process(delta):
-	if abs(player.get_global_pos().y - self.get_global_pos().y) > MIN_DISTANCE:
+	var player_pos = player.get_global_pos()
+	if abs(player_pos.y - self.get_global_pos().y) > MIN_DISTANCE:
 		move(Vector2(0, -SPEED * 8))
+	elif -player_pos.y > DEATH_DISTANCE:
+		move(Vector2(0, -SPEED * 2))
 	else:
 		move(Vector2(0, -SPEED))
 
